145b201f56eb45bda728e02a965c36e887a56cb0
Roger Ye <shugaoye@yahoo.com>
2022-01-09 00:09:50 +0800

    Updated to Version 17.1.0 Preview 2.0

--- PassXYZ.Vault/Platforms/MacCatalyst/AppDelegate.cs
+++ PassXYZ.Vault/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,12 +1,9 @@
 ﻿using Foundation;
-using Microsoft.Maui;
-using Microsoft.Maui.Hosting;
 
-namespace PassXYZ.Vault
+namespace PassXYZ.Vault;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
 {
-	[Register("AppDelegate")]
-	public class AppDelegate : MauiUIApplicationDelegate
-	{
-		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-	}
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
