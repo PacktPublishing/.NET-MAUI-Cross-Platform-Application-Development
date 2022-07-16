2d3b180c3896ab3532a063998aa98ba53fa5f822
Roger Ye <shugaoye@yahoo.com>
2022-07-16 23:15:27 +0800

    Support data binding in ItemDetail.razor

--- PassXYZ.Vault/Pages/ItemDetail.razor.cs
+++ PassXYZ.Vault/Pages/ItemDetail.razor.cs
@@ -25,6 +25,22 @@ namespace PassXYZ.Vault.Pages
 
         Item? selectedItem = default!;
 
+        private string newKey = string.Empty;
+        private string newValue = string.Empty;
+        private bool IsPassword = false;
+        private Field? listGroupField = default!;
+        private string listGroupFieldKey => ((listGroupField != null) ? listGroupField.Key : "");
+        private string listGroupFieldValue
+        { 
+            get 
+            { 
+                if (listGroupField == null) { return ""; }
+                if (listGroupField.IsProtected) { return listGroupField.EditValue; }
+                return listGroupField.Value; 
+            }
+        }
+        bool IsKeyEditingEnable = false;
+
         public ItemDetail()
         {
             fields = new ObservableCollection<Field>();
@@ -65,5 +81,59 @@ namespace PassXYZ.Vault.Pages
                 throw new InvalidOperationException("ItemDetail: SelectedItemId is null");
             }
         }
+
+        private void OnValueChanged(ChangeEventArgs e)
+        {
+            if (e.Value == null)
+            {
+                Debug.WriteLine("ItemDetail.OnValueChanged: ChangeEventArgs is null");
+            }
+            else
+            {
+                if (listGroupField == null || IsKeyEditingEnable)
+                {
+                    newValue = e.Value.ToString();
+                    Debug.WriteLine($"ItemDetail.OnValueChanged: New Value={newValue}");
+                }
+                else
+                {
+                    listGroupField.Value = e.Value.ToString();
+                    Debug.WriteLine($"ItemDetail.OnValueChanged: Value={listGroupField.Value}");
+                }
+            }
+        }
+
+        private void OnToggleShowPassword(MouseEventArgs e) 
+        {
+            if (listGroupField == null) return;
+
+            if (listGroupField.IsHide)
+            {
+                listGroupField.ShowPassword();
+            }
+            else 
+            {
+                listGroupField.HidePassword();
+            }
+        }
+
+        private void UpdateField(MouseEventArgs e)
+        {
+            if (listGroupField == null || IsKeyEditingEnable)
+            {
+                Debug.WriteLine($"ItemDetail.UpdateField: New Key={newKey}, New Value={newValue}, IsPassword={IsPassword}");
+            }
+            else 
+            {
+                Debug.WriteLine($"ItemDetail.UpdateField: Key={listGroupField.Key}, Value={listGroupField.Value}");
+            }
+        }
+
+        private void DeleteField(MouseEventArgs e)
+        {
+            if (listGroupField == null) return;
+            Debug.WriteLine($"ItemDetail.DeleteField: Key={listGroupField.Key}, Value={listGroupField.Value}");
+        }
+
     }
 }
