6a2ce057d14bfc34e36b7be4e99c9669875ab7fd
Roger Ye <shugaoye@yahoo.com>
2022-07-22 10:07:22 +0800

    Added BlazorUI and bUnit

---
+++ PassXYZ.BlazorUI/EditorDialog.razor.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace PassXYZ.BlazorUI;
+
+public partial class EditorDialog
+{
+    [Parameter]
+    public string? Id { get; set; }
+    [Parameter]
+    public string Title { get; set; } = "editModal";
+    [Parameter]
+    public bool IsKeyEditingEnable { get; set; }
+
+    string _key = string.Empty;
+    [Parameter]
+    public string Key
+    {
+        get => _key;
+        set
+        {
+            _key = value;
+            KeyChanged?.InvokeAsync(_key);
+            Debug.WriteLine($"EditorDialog: Key={_key}");
+        }
+    }
+    [Parameter]
+    public EventCallback<string>? KeyChanged { get; set; }
+
+    string _value = string.Empty;
+    [Parameter]
+    public string Value 
+    {
+        get => _value;
+        set 
+        {
+            _value = value ?? string.Empty;
+            ValueChanged?.InvokeAsync(_value);
+        } 
+    }
+    [Parameter]
+    public EventCallback<string>? ValueChanged { get; set; }
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+
+    [Parameter]
+    public Action<string, string>? OnSave { get; set; }
+
+    async Task<bool> OnSaveClicked()
+    {
+        //TestField.Key = key;
+        //TestField.Value = value;
+        OnSave?.Invoke(Key, Value);
+        Debug.WriteLine($"EditorDialog: OnSaveClicked(key={Key}, value={Value})");
+        return true;
+    }
+}
---
+++ PassXYZ.BlazorUI/ModalDialog.razor.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+namespace PassXYZ.BlazorUI;
+
+public partial class ModalDialog 
+{
+    [Parameter]
+    public string? Id { get; set; }
+    [Parameter]
+    public string? Title { get; set; }
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+    [Parameter]
+    public Func<Task>? OnClose { get; set; }
+    [Parameter]
+    public Func<Task<bool>>? OnSaveAsync { get; set; }
+    [Parameter]
+    [NotNull]
+    public string? CloseButtonText { get; set; }
+    [Parameter]
+    [NotNull]
+    public string? SaveButtonText { get; set; }
+
+    private async Task OnClickClose()
+    {
+        if (OnClose != null)
+        {
+            await OnClose();
+        }
+    }
+
+    private async Task OnClickSave() 
+    {
+        if (OnSaveAsync != null)
+        {
+            await OnSaveAsync();
+        }
+    }
+}
