cfb8cc982f22127af4514ada5320e23d8ca73034
Roger Ye <shugaoye@yahoo.com>
2023-04-22 22:26:37 +0800

    Added BaseViewModel

---
+++ PassXYZ.Vault/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassXYZ.Vault.ViewModels
+{
+    public abstract partial class BaseViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private void ItemSelectionChanged(object sender)
+        {
+            OnItemSelecteion(sender);
+        }
+
+        public abstract void OnItemSelecteion(object sender);
+    }
+}
