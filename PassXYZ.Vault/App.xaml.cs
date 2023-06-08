2b155c13fe6cd591dec9c911373c5380ebea3f0c
Roger Ye <shugaoye@yahoo.com>
2023-06-08 15:45:46 +0800

    Fixed compilation warnings

--- PassXYZ.Vault/App.xaml.cs
+++ PassXYZ.Vault/App.xaml.cs
@@ -41,20 +41,20 @@ public partial class App : Application
         InBackground = true;
 
         // Lock screen after timeout
-        Device.StartTimer(TimeSpan.FromSeconds(PxUser.AppTimeout), () =>
+        if(Current != null) 
         {
-            if (InBackground)
-            {
+            var timer = Current.Dispatcher.CreateTimer();
+            timer.Interval = TimeSpan.FromSeconds(PxUser.AppTimeout);
+            timer.Tick += (s, e) => {
+                if (InBackground)
+                {
 
-                // TODO: dataStore.Logout();
-                _isLogout = true;
-                return false;
-            }
-            else
-            {
-                return false;
-            }
-        });
+                    // TODO: dataStore.Logout();
+                    _isLogout = true;
+                }
+            };
+            timer.Start();
+        }
     }
 
     protected override void OnResume()
@@ -82,6 +82,7 @@ public partial class App : Application
                 using (var stream = assembly.GetManifestResourceStream(iconFile.ResourcePath))
                 using (var fileStream = new FileStream(iconFile.Path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
                 {
+                    if(stream == null) { throw new NullReferenceException("stream is null"); }
                     stream.CopyTo(fileStream);
                 }
             }
@@ -92,6 +93,7 @@ public partial class App : Application
             using (var stream = assembly.GetManifestResourceStream(EmbeddedIcons.iconZipFile.ResourcePath))
             using (var fileStream = new FileStream(EmbeddedIcons.iconZipFile.Path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
             {
+                if (stream == null) { throw new NullReferenceException("stream is null"); }
                 stream.CopyTo(fileStream);
             }
             ZipFile.ExtractToDirectory(EmbeddedIcons.iconZipFile.Path, PxDataFile.IconFilePath);
@@ -109,6 +111,7 @@ public partial class App : Application
                 using (var stream = assembly.GetManifestResourceStream(eDb.ResourcePath))
                 using (var fileStream = new FileStream(eDb.Path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
                 {
+                    if (stream == null) { throw new NullReferenceException("stream is null"); }
                     stream.CopyTo(fileStream);
                 }
             }
